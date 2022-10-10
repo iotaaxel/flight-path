40af23acaf8bc3da25f15508fc75b7f5013cb0e9
Axel Iota <iota.axel@gmail.com>
2022-10-10 01:27:47 -0400

    Basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    // #TODO:
+    // 1. Take in list of flight paths and process JSON format
+    // 2. Split flight paths list into BTreeSet or two HashSets for source and dest airport codes
+    // 3. Use symmetric difference to ignore shared airport codes
+
+    //input: [['IND', 'EWR'], ['SFO', 'ATL'], ['GSO', 'IND'], ['ATL', 'GSO']]
+    //source_codes: ['IND', 'SFO', 'GSO', 'ATL']
+    //dest_codes: ['EWR', 'ATL', 'IND', 'GSO']
+    //trip_path: ['SFO', 'EWR']
+    let source_codes = HashSet::from(["IND", "SFO", "GSO", "ATL"]);
+    let dest_codes = HashSet::from(["EWR", "ATL", "IND", "GSO"]);
+
+    let trip_path = source_codes.symmetric_difference(&dest_codes);
+    println!("{:?}", trip_path);
 }
